a68cff7d6619c0752beeee9f6bd6c511e0e20bb0
amirga <amirgafni@gmail.com>
2014-05-23 15:14:04 -0700

    created tb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tb::Application.config.secret_token = '8fa9cb8b3b5e4660d6ba025353bd633edcd1528e021c0d482dea7af650d7d0c1745ed6a6a0c70ab3e66934ae10f9afeab8dede7a2d709b64e8a9a08755f39e91'
